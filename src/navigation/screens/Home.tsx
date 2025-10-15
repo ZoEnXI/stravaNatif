39cac9cb31de1d853152f8343863164f542d7575
lavau <lavaud.enzo@gmail.com>
2025-10-15 15:09:42 +0200

    config firebase

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,17 +1,40 @@
 import { Button, Text } from '@react-navigation/elements';
 import { StyleSheet, View } from 'react-native';
+import {useEffect, useState} from "react";
+import auth, {FirebaseAuthTypes, getAuth, onAuthStateChanged} from "@react-native-firebase/auth";
 
 export function Home() {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Text>Open up 'src/App.tsx' to start working on your app!</Text>
-      <Button screen="Profile" params={{ user: 'jane' }}>
-        Go to Profile
-      </Button>
-      <Button screen="Settings">Go to Settings</Button>
-    </View>
-  );
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+    useEffect(() => {
+        auth().onAuthStateChanged(userState => {
+            setUser(userState);
+
+            if (loading) {
+                setLoading(false);
+            }
+        });
+    }, []);
+
+    if (!user) {
+        return (
+            <View>
+                <Text>Login</Text>
+            </View>
+        );
+    }
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen</Text>
+            <Text>Open up 'src/App.tsx' to start working on your app!</Text>
+            <Button screen="Profile" params={{ user: 'jane' }}>
+                Go to Profile
+            </Button>
+            <Button screen="Settings">Go to Settings</Button>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
